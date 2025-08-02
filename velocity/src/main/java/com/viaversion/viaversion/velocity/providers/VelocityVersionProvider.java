e0dd9594b11cb14459e726b9242fef0821a26bff
zzniki <39004156+zzniki@users.noreply.github.com>
2025-08-02 17:00:39 +0200

    Update VelocityVersionProvider.java

--- velocity/src/main/java/com/viaversion/viaversion/velocity/providers/VelocityVersionProvider.java
+++ velocity/src/main/java/com/viaversion/viaversion/velocity/providers/VelocityVersionProvider.java
@@ -52,7 +52,8 @@ public class VelocityVersionProvider implements VersionProvider {
         //TODO use newly added Velocity netty event
         ChannelHandler mcHandler = user.getChannel().pipeline().get("handler");
         ServerConnection serverConnection = (ServerConnection) GET_ASSOCIATION.invoke(mcHandler);
-        return Via.proxyPlatform().protocolDetectorService().serverProtocolVersion(serverConnection.getServerInfo().getName());
+        return ProtocolVersion.getProtocol(340);
+        //return Via.proxyPlatform().protocolDetectorService().serverProtocolVersion(serverConnection.getServerInfo().getName());
     }
 
     private ProtocolVersion getFrontProtocol(UserConnection user) throws Exception {
